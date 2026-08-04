5d8121bb9c139ef86d57807de0fe02587f0f9fc6
Колотилкин Данил <d.kolotilkin@zebrains.team>
2026-08-04 18:18:58 +0300

    add throws in interface

--- src/Domain/Services/RoleResolverInterface.php
+++ src/Domain/Services/RoleResolverInterface.php
@@ -2,6 +2,7 @@
 
 namespace Histel951\Dota2Api\Domain\Services;
 
+use Histel951\Dota2Api\Domain\Common\Exceptions\DomainException;
 use Histel951\Dota2Api\Domain\Entities\Match\ValueObjects\MatchPlayerPerformance;
 
 interface RoleResolverInterface
@@ -10,6 +11,7 @@ interface RoleResolverInterface
      * @param MatchPlayerPerformance[] $players
      * @param bool $isRadiant нужен, поскольку линия отдаётся с апи только со стороны radiant, поэтому safe lane для dire будет считаться как offlane
      * @return MatchPlayerPerformance[]
+     * @throws DomainException
      */
     public function resolve(array $players, bool $isRadiant = true): array;
 }
